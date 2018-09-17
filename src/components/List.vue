95bdc3217951c8580d36e0adb029cc37af7d1a0a
wangjinkai <wangjinkai@guazi.com>
2018-09-17 23:59:20 +0800

    feat: 编辑区样式

--- src/components/List.vue
+++ src/components/List.vue
@@ -9,9 +9,7 @@
         </mu-list>
       </mu-flex>
       <mu-flex class="note-content">
-        <div v-if="!!$root.currentNoteContent.id">
-          <input :value="$root.currentNoteContent.content" type="text" class="content-input" @change="contentChange"/>
-        </div>
+        <textarea v-if="!!$root.currentNoteContent.id" name="text" :value="$root.currentNoteContent.content" class="content-input" rows="4" placeholder="编写笔记..."></textarea>
         <div v-else class="bg-box">
           <p class="note-mark">{{$root.noteList.length}}篇笔记</p> 
         </div>
@@ -20,66 +18,71 @@
   </div>
 </template>
 <script>
-
 export default {
-  data () {
+  data() {
     return {
-      name: 'list',
-      currentTitle: '',
-      currentContent: ''
-    }
+      name: "list",
+      currentTitle: "",
+      currentContent: ""
+    };
   },
-  mounted () {
-   
-  },
+  mounted() {},
   methods: {
-    updateNoteId (id) {
-      this.$root.currentNoteId = id
+    updateNoteId(id) {
+      this.$root.currentNoteId = id;
     },
-    getStyle (id) {
+    getStyle(id) {
       return {
-        'background': this.$root.currentNoteId == id ? this.$root.color[1] : 'transparent'
-      }
+        background:
+          this.$root.currentNoteId == id ? this.$root.color[1] : "transparent"
+      };
     },
-    contentChange (value) {
-      console.log(value)
+    contentChange(value) {
+      console.log(value);
     }
   }
-}
+};
 </script>
 
 <style lang="less" scoped>
- .list {
-   .list-wrapper {
-     .note-title {
-       min-width: 200px;
-       border-right: 1px solid #b0bec5;
-     }
-     .note-content {
-        margin: 10px;
+.list {
+  .list-wrapper {
+    .note-title {
+      min-width: 200px;
+      border-right: 1px solid #b0bec5;
+    }
+    .note-content {
+      margin: 10px;
+      width: 100%;
+      height: 100%;
+      .content-input {
         width: 100%;
         height: 100%;
-        .content-input {
-          &:focus {
-            border: none;
-            outline-offset: 0;
-            outline-color: #fff;
-          }
+        border: none;
+        outline-offset: 0;
+        outline-color: #fafafa;
+        background-color: #fafafa;
+        resize: none;
+        &:focus {
+          border: none;
+          outline-offset: 0;
+          outline-color: #fafafa;
         }
-        .bg-box {
+      }
+      .bg-box {
+        width: 100%;
+        height: 100%;
+        background: no-repeat center url("../assets/maple.svg");
+        opacity: 0.4;
+        position: relative;
+        .note-mark {
+          position: absolute;
+          bottom: 50px;
           width: 100%;
-          height: 100%;
-          background: no-repeat center url('../assets/maple.svg');
-          opacity: .4;
-          position: relative;
-          .note-mark {
-            position: absolute;
-            bottom: 50px;
-            width: 100%;
-            text-align: center;
-          }
+          text-align: center;
         }
-     }
-   }
- }
+      }
+    }
+  }
+}
 </style>
